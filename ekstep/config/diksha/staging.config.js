0946ce608ed28c4260aacaf9db777049358fa615
rak16 <ravishahmad16@gmail.com>
2019-03-25 15:42:16 +0530

    Change the server IP address in the config files

--- ekstep/config/diksha/staging.config.js
+++ ekstep/config/diksha/staging.config.js
@@ -6,11 +6,11 @@ module.exports = {
    "FS_ROOT": "/home/admin/",
    "username": "brandonStark",
    "password": "theBuilder",
-   "serverIP": "35.240.131.174",
+   "serverIP": "35.198.199.222",
    "telemetryAPI": {
-       "init": "http://35.240.131.174:8888/api/auth/v1/init", // Authenticates user and returns a token
-       "hello": "http://35.240.131.174:8000/api/auth/v1/hello", // Checks if the user is authenticated
-       "upload": "http://35.240.131.174:8000/api/auth/v1/telemetry/couchbase" // Load telemetry
+       "init": "http://35.198.199.222:8888/api/auth/v1/init", // Authenticates user and returns a token
+       "hello": "http://35.198.199.222:8000/api/auth/v1/hello", // Checks if the user is authenticated
+       "upload": "http://35.198.199.222:8000/api/auth/v1/telemetry/couchbase" // Load telemetry
    },
    "keyFile": "/etc/telemetry_key",
 
